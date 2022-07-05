d97474b34775b9c422f13ba5a69925cc98bd0880
adityamotwani <adityamotwani4@gmail.com>
2022-07-05 12:21:11 +0530

    Added website by Shivani + Changes in Team section

---
+++ js/data.js
@@ -0,0 +1,23 @@
+export const internData = {
+    key: 'Intern',
+}
+
+export const researchData = [{
+    heading: 'Cords',
+    content: [{
+        main: 'Submodularity in data subset selection and active learning',
+        subContent: 'Kai Wei, Rishabh Iyer, Jeff Bilmes <br /> International Conference on Machine Learning (ICML) 2015',
+    }, {
+        main:'Learning From Less Data: A Unified Data Subset Selection and Active Learning Framework for Computer Vision',
+        subContent: 'Vishal Kaushal, Rishabh Iyer, Suraj Kothiwade, Rohan Mahadev, Khoshrav Doctor, and Ganesh Ramakrishnan <br /> 7th IEEE Winter Conference on Applications of Computer Vision (WACV), 2019 Hawaii, USA',
+    }],
+}, {
+    heading: 'DISTIL',
+    content: [{
+        main: 'Submodularity in data subset selection and active learning',
+        subContent: 'Kai Wei, Rishabh Iyer, Jeff Bilmes <br /> International Conference on Machine Learning (ICML) 2015',
+    }, {
+        main: 'Deep batch active learning by diverse, uncertain gradient lower bounds.',
+        subContent: 'Ash, Jordan T., et al. <br /> 8th International Conference on Learning Representations (ICLR), 2020',
+    }],
+}]
